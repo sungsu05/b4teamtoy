d91ca3afc96250abdc5ffd5f701e4117bff9bb8a
Bookwhale00 <Bookwhale00@gmail.com>
2023-05-09 14:52:09 +0900

    :art: Style: posts urls, views 틀 구성, Pillow패키지추가
    아직 마이그레이션 안 한 상태임

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from posts.models import Post
 
-# Create your views here.
+
+
+# 게시글 메인, 작성 뷰(get, post)
+class PostView(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+# 게시글 상세보기, 수정, 삭제 뷰(get, put, delete)
+class PostDetailView(APIView):
+    def get(self, request, post_id):
+        pass
+    
+    def put(self, request, post_id):
+        pass
+    
+    def delete(self, request, post_id):
+        pass
