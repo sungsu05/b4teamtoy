b7e600c610097f0a0ec3245e113e598bc55546fa
leejunghyun <joa3406@gmail.com>
2023-05-09 18:08:36 +0900

    :sparkles: comment 테이터베이스 #4
    확인 부타드립니다

---
+++ comments/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from comments.models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+# from rest_framework.views import APIView
+# from rest_framework import status
+# from rest_framework.response import Response
+# from comments.models import Comment
+# from posts.models import Post
 
-# Create your views here.
+# # 댓글
+# class CommentView(APIView):
+# 	def get(self, request, post_id):
+# 		posts = Post.objects.get(id=post_id)
+#         comments = posts.comment_set.all()
+#         serializer = CommentSerializer(comments,many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+  	
+#     def post(self, request, post_id):
+#         pass
+    	   
+
+# 댓글 수정 삭제
+# class CommnentDetailView(APIView):
+# 	def put(self, request, post_id):
+# 		pass
+    	 
+#     def del(self, request, post_id):
+#         pass
+
+# posts id를 몰라서 틀만 잡고 하는중이였습니다!    	   
