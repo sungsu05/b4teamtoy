f14ead249877eeba7f6bdc024ac5203c6af3852c
jstyoon <103176409+jstyoon@users.noreply.github.com>
2023-05-12 15:18:52 +0900

    Update urls.py

--- comments/urls.py
+++ comments/urls.py
@@ -3,6 +3,6 @@ from comments import views
 
 urlpatterns = [
     # path('',views.CommentView.as_view(),name='comment_view'),영주님의 경로잡기
-    path("<int:post_id>/comment/", views.CommentView.as_view(),name='comment_view'),
+    path("<int:post_id>/comment", views.CommentView.as_view(),name='comment_view'),
     path("<int:post_id>/comment/<int:comment_id>/", views.CommentDetailView.as_view(),name='comment_detail_view'),
 ]
