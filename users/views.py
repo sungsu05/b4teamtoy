dc308ded4b5b782c25a6cb05d54b779ef23e2ef1
SungSuSon <thstjdtn05@gmail.com>
2023-05-14 17:47:06 +0900

    :bug:Fix: update users app vews.py
    병합 충돌 해결

--- users/views.py
+++ users/views.py
@@ -13,13 +13,8 @@ import random
 # 회원 가입
 
 
-<<<<<<< HEAD
 class AuthFuntion():
-    def send_mail(self, email):
-=======
-class AuthFunction():
     def send_mail(self,email):
->>>>>>> 9d1998601a6fde37d7669031b8f7a78a5582ed1e
         code = "".join([str(random.randrange(0, 10)) for i in range(6)])
         title = "B4GAMES 가입 인증 코드 발송"
         content = f"인증 코드 = {code}"
@@ -48,40 +43,8 @@ class AuthFunction():
         return True
 
 
-<<<<<<< HEAD
-class SignUp(APIView, AuthFuntion):
-    def send_mail(self, email):
-=======
-class SignUp(APIView,AuthFunction):
-    def send_mail(self,email):
->>>>>>> 9d1998601a6fde37d7669031b8f7a78a5582ed1e
-        code = "".join([str(random.randrange(0, 10)) for i in range(6)])
-        title = "B4GAMES 가입 인증 코드 발송"
-        content = f"인증 코드 = {code}"
-        mail = EmailMessage(title, content, to=[email])
-        mail.send()
 
-        # 올바른 이메일로 발송했는지 검증 절차 필요
-        return code
-
-    def check_password(self, password):
-        check = [
-            lambda element: all(
-                x.isdigit() or x.islower() or x.isupper() or (x in ['!', '@', '#', '$', '%', '^', '&', '*', '_']) for x
-                in element),
-            # 요소 하나 하나를 순환하며 숫자,소문자,대문자,지정된 특수문자 제외한 요소가 있을경우 False
-            lambda element: len(element) == len(element.replace(" ", "")),
-            # 공백이 포함 되어 있을 경우 False
-            lambda element: True if (len(element) > 7 and len(element) < 21) else False,
-            # 전달된 값의 개수가 8~20 사이일 경우 True
-            lambda element: any(x.islower() or x.isupper() for x in element),
-            # 요소 하나하나를 순환하며, 요소중 대문자 또는 소문자가 있어야함(숫자로만 가입 불가능)
-        ]
-        for i in check:
-            if not i(password):
-                return False
-        return True
-
+class SignUp(APIView,AuthFuntion):
     def post(self, request):
         if not self.check_password(request.data['password']):
             return Response({'error': '비밀번호가 올바르지 않습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
@@ -113,12 +76,7 @@ class SignUp(APIView,AuthFunction):
 
         return Response({"error": "인증 코드가 올바르지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-<<<<<<< HEAD
-
 class UserView(APIView, AuthFuntion):
-=======
-class UserView(APIView,AuthFunction):
->>>>>>> 9d1998601a6fde37d7669031b8f7a78a5582ed1e
     # 회원 정보 읽기
     def get(self, request, user_id):
         owner = get_object_or_404(User, id=user_id)
@@ -202,8 +160,6 @@ class ProfileView(APIView):
         else:
             return Response({"error": "권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-<<<<<<< HEAD
-
 class GetAuthCode(APIView, AuthFuntion):
     def post(self, request):
         owner = get_object_or_404(User, email=request.data['email'])
@@ -211,12 +167,3 @@ class GetAuthCode(APIView, AuthFuntion):
         owner.save()
         print(owner.auth_code)
         return Response({"message": "인증 메일을 발송 했습니다."}, status=status.HTTP_200_OK)
-=======
-class GetAuthCode(APIView,AuthFunction):
-    def post(self,request):
-        owner = get_object_or_404(User,email=request.data['email'])
-        owner.auth_code = self.send_mail(owner.email)
-        owner.save()
-        print(owner.auth_code)
-        return Response({"message":"인증 메일을 발송 했습니다."}, status=status.HTTP_200_OK)
->>>>>>> 9d1998601a6fde37d7669031b8f7a78a5582ed1e
