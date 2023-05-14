647696271f4af728eff0daa3f1cf39eea08322bc
Yi Yoonsung <lys11556633@gmail.com>
2023-05-14 14:04:51 +0900

    :Rename: posts get_owner메서드  email -> username 변경

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,7 +6,7 @@ class PostlistSerializer (serializers.ModelSerializer):
     owner = serializers.SerializerMethodField()
     
     def get_owner(self, obj):
-        return obj.owner.email
+        return obj.owner.username
     
     class Meta:
         model = Post
@@ -24,7 +24,7 @@ class PostDetailSerializer (serializers.ModelSerializer):
     owner = serializers.SerializerMethodField()
     
     def get_owner(self, obj):
-        return obj.owner.email
+        return obj.owner.username
     class Meta:
         model = Post
         fields = ("pk", "owner", "title", "image", "content")
