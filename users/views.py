ab4baec229e4b9cd50200a169df0d01e2ef5d92f
SungSuSon <thstjdtn05@gmail.com>
2023-05-14 17:43:01 +0900

    step

--- users/views.py
+++ users/views.py
@@ -4,27 +4,31 @@ from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from .models import User
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import (UserSerializer,ComtomTokenObtainPairSerializer,ReadUserSerializer,
-                          ReadProfileSerializer,UpdateProfileSerializer,GetFollowInfoSerializer)
+from .serializers import (UserSerializer, ComtomTokenObtainPairSerializer, ReadUserSerializer,
+                          ReadProfileSerializer, UpdateProfileSerializer, GetFollowInfoSerializer)
 from django.core.mail import EmailMessage
 import random
+
+
 # 회원 가입
 
 
 class AuthFuntion():
-    def send_mail(self,email):
+    def send_mail(self, email):
         code = "".join([str(random.randrange(0, 10)) for i in range(6)])
         title = "B4GAMES 가입 인증 코드 발송"
         content = f"인증 코드 = {code}"
-        mail = EmailMessage(title,content,to=[email])
+        mail = EmailMessage(title, content, to=[email])
         mail.send()
 
         # 올바른 이메일로 발송했는지 검증 절차 필요
         return code
 
-    def check_password(self,password):
+    def check_password(self, password):
         check = [
-            lambda element: all(x.isdigit() or x.islower() or x.isupper() or (x in ['!', '@', '#', '$', '%', '^', '&', '*', '_']) for x in element),
+            lambda element: all(
+                x.isdigit() or x.islower() or x.isupper() or (x in ['!', '@', '#', '$', '%', '^', '&', '*', '_']) for x
+                in element),
             # 요소 하나 하나를 순환하며 숫자,소문자,대문자,지정된 특수문자 제외한 요소가 있을경우 False
             lambda element: len(element) == len(element.replace(" ", "")),
             # 공백이 포함 되어 있을 경우 False
@@ -39,20 +43,22 @@ class AuthFuntion():
         return True
 
 
-class SignUp(APIView,AuthFuntion):
-    def send_mail(self,email):
+class SignUp(APIView, AuthFuntion):
+    def send_mail(self, email):
         code = "".join([str(random.randrange(0, 10)) for i in range(6)])
         title = "B4GAMES 가입 인증 코드 발송"
         content = f"인증 코드 = {code}"
-        mail = EmailMessage(title,content,to=[email])
+        mail = EmailMessage(title, content, to=[email])
         mail.send()
 
         # 올바른 이메일로 발송했는지 검증 절차 필요
         return code
 
-    def check_password(self,password):
+    def check_password(self, password):
         check = [
-            lambda element: all(x.isdigit() or x.islower() or x.isupper() or (x in ['!', '@', '#', '$', '%', '^', '&', '*', '_']) for x in element),
+            lambda element: all(
+                x.isdigit() or x.islower() or x.isupper() or (x in ['!', '@', '#', '$', '%', '^', '&', '*', '_']) for x
+                in element),
             # 요소 하나 하나를 순환하며 숫자,소문자,대문자,지정된 특수문자 제외한 요소가 있을경우 False
             lambda element: len(element) == len(element.replace(" ", "")),
             # 공백이 포함 되어 있을 경우 False
@@ -66,16 +72,16 @@ class SignUp(APIView,AuthFuntion):
                 return False
         return True
 
-    def post(self,request):
+    def post(self, request):
         if not self.check_password(request.data['password']):
-            return Response({'error':'비밀번호가 올바르지 않습니다.'},status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'error': '비밀번호가 올바르지 않습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
 
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             # email로 인증 코드 발송, owner의 데이터베이스에 인증 코드 저장
             serializer.save()
             email = serializer.validated_data.get('email')
-            owner = get_object_or_404(User,email=email)
+            owner = get_object_or_404(User, email=email)
             owner.auth_code = self.send_mail(email)
             # if owner.auth_code == None:
             #     owner.delete()
@@ -83,90 +89,93 @@ class SignUp(APIView,AuthFuntion):
 
             owner.save()
 
-            return Response({'message':f'가입을 축하합니다.'},status=status.HTTP_201_CREATED)
+            return Response({'message': f'가입을 축하합니다.'}, status=status.HTTP_201_CREATED)
         else:
-            return Response({'message':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self,request):
+    def put(self, request):
         owner = get_object_or_404(User, email=request.data['email'])
         print(owner.auth_code)
         if owner.auth_code == request.data['auth_code']:
             owner.is_active = True
             owner.save()
-            return Response({"message":"인증되셨습니다."}, status=status.HTTP_200_OK)
+            return Response({"message": "인증되셨습니다."}, status=status.HTTP_200_OK)
 
-        return Response({"error":"인증 코드가 올바르지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"error": "인증 코드가 올바르지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-class UserView(APIView,AuthFuntion):
+
+class UserView(APIView, AuthFuntion):
     # 회원 정보 읽기
-    def get(self,request,user_id):
-        owner = get_object_or_404(User,id=user_id)
+    def get(self, request, user_id):
+        owner = get_object_or_404(User, id=user_id)
         serializer = ReadUserSerializer(owner)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     # 회원 정보 수정
-    def put(self,request,user_id):
+    def put(self, request, user_id):
         if not self.check_password(request.data['password']):
-            return Response({'error':'비밀번호가 올바르지 않습니다.'},status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'error': '비밀번호가 올바르지 않습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
 
-        owner = get_object_or_404(User,id=user_id)
+        owner = get_object_or_404(User, id=user_id)
         print(owner.auth_code)
         if not request.data['auth_code'] == owner.auth_code:
-            return Response({'error':'인증 코드가 올바르지 않습니다.'},status=status.HTTP_421_MISDIRECTED_REQUEST)
+            return Response({'error': '인증 코드가 올바르지 않습니다.'}, status=status.HTTP_421_MISDIRECTED_REQUEST)
 
         if request.user == owner:
             serializer = UserSerializer(owner,data=request.data,partial=True)
             # partial=True : 부분 업데이트
             if serializer.is_valid():
                 serializer.save()
-    
+
                 update_user_info = ReadUserSerializer(owner)
                 return Response(update_user_info.data,status=status.HTTP_200_OK)
             else:
                 return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({"error":"권한이 없습니다."},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
     # 휴면 계정으로 전환
-    def delete(self,request,user_id):
-        owner = get_object_or_404(User,id=user_id)
+    def delete(self, request, user_id):
+        owner = get_object_or_404(User, id=user_id)
         if request.user == owner:
             owner = get_object_or_404(User, id=user_id)
             owner.is_active = False
             owner.save()
-            return Response({"message":"휴면 계정으로 전환되었습니다."},status=status.HTTP_200_OK)
+            return Response({"message": "휴면 계정으로 전환되었습니다."}, status=status.HTTP_200_OK)
         else:
-            return Response({"error":"권한이 없습니다."},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = ComtomTokenObtainPairSerializer
 
+
 class ProfileView(APIView):
     # owner의 프로필 읽기 (public)
-    def get(self,request,user_id):
-        owner = get_object_or_404(User,id=user_id)
+    def get(self, request, user_id):
+        owner = get_object_or_404(User, id=user_id)
         serializer = ReadProfileSerializer(owner)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     # owner를 팔로우,언팔로우
-    def post(self,request,user_id):
+    def post(self, request, user_id):
         # if not request.user.is_authenticated:
         #     return Response("로그인이 필요합니다.", status=status.HTTP_401_UNAUTHORIZED)
 
-        owner = get_object_or_404(User,id=user_id)
+        owner = get_object_or_404(User, id=user_id)
 
         if request.user == owner:
             return Response("나 자신을 팔로우 할 수 없습니다.", status=status.HTTP_400_BAD_REQUEST)
 
         if request.user in owner.followers.all():
             owner.followers.remove(request.user)
-            return Response("unfollow",status=status.HTTP_200_OK)
+            return Response("unfollow", status=status.HTTP_200_OK)
         else:
             owner.followers.add(request.user)
-            return Response("follow",status=status.HTTP_200_OK)
+            return Response("follow", status=status.HTTP_200_OK)
 
     # owner가 자신의 프로필을 수정
-    def put(self,request,user_id):
+    def put(self, request, user_id):
         owner = get_object_or_404(User, id=user_id)
         if request.user == owner:
             serializer = UpdateProfileSerializer(owner, data=request.data, partial=True)
@@ -179,10 +188,11 @@ class ProfileView(APIView):
         else:
             return Response({"error": "권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-class GetAuthCode(APIView,AuthFuntion):
-    def post(self,request):
-        owner = get_object_or_404(User,email=request.data['email'])
+
+class GetAuthCode(APIView, AuthFuntion):
+    def post(self, request):
+        owner = get_object_or_404(User, email=request.data['email'])
         owner.auth_code = self.send_mail(owner.email)
         owner.save()
         print(owner.auth_code)
-        return Response({"message":"인증 메일을 발송 했습니다."}, status=status.HTTP_200_OK)
+        return Response({"message": "인증 메일을 발송 했습니다."}, status=status.HTTP_200_OK)
